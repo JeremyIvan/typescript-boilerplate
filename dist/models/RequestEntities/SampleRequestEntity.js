471988f7c3fed86666b8a412ce2a145e9b5fb0b1
JeremyIvan <41867589+JeremyIvan@users.noreply.github.com>
2024-07-29 15:45:17 +0800

    Remove dist folder from commit

--- dist/models/RequestEntities/SampleRequestEntity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SampleRequestEntity = void 0;
-class SampleRequestEntity {
-}
-exports.SampleRequestEntity = SampleRequestEntity;
--- dist/models/ResponseEntities/SampleResponseEntity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SampleResponseEntity = void 0;
-class SampleResponseEntity {
-}
-exports.SampleResponseEntity = SampleResponseEntity;
